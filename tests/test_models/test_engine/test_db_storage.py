7e9297b55b1fbdb01690d57006e7d2e046fd600b
Abrar Saad <abrarsaad3572022@gmail.com>
2024-05-25 22:11:43 +0300

    2. Improve storage

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -86,3 +86,30 @@ class TestFileStorage(unittest.TestCase):
     @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_save(self):
         """Test that save properly saves objects to file.json"""
+
+
+class TestDBStorage(unittest.TestCase):
+    """Test the FileStorage class"""
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get(self):
+        """Test that get returns specific object, or not"""
+        newState = State(name="Alexandria")
+        newState.save()
+        newUser = User(email="abrarsaad917@gmail.com", password="password")
+        newUser.save()
+        self.assertIs(newState, models.storage.get("State", newState.id))
+        self.assertIs(None, models.storage.get("State", "abrar"))
+        self.assertIs(None, models.storage.get("State", "anan"))
+        self.assertIs(newUser, models.storage.get("User", newUser.id))
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """add new object to DB"""
+        Start = models.storage.count()
+        self.assertEqual(models.storage.count("Abrar"), 0)
+        newState = State(name="Aswan")
+        newState.save()
+        newUser = User(email="abrarsaad3572022@gmail.com", password="password")
+        newUser.save()
+        self.assertEqual(models.storage.count("State"), Start + 1)
+        self.assertEqual(models.storage.count(), Start + 2)
