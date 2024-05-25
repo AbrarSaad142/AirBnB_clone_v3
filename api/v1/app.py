62945c589ae6d20af5d5711323b58d0bb4645177
Areejslama <Areegahmed449@gmail.com>
2024-05-25 15:22:18 -0700

    3

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,8 +5,8 @@ from models import storage
 import os
 from api.v1.views import app_views
 
-port= os.getenv("HBNB_API_PORT", "5000")
-host= os.getenv("HBNB_API_HOST", "0.0.0.0")
+port = os.getenv("HBNB_API_PORT", "5000")
+host = os.getenv("HBNB_API_HOST", "0.0.0.0")
 
 
 app = Flask(__name__)
