bc2bf76f74b7e3b7cc417ab5e3019dacc881b611
Abrar Saad <abrarsaad3572022@gmail.com>
2024-05-27 16:46:30 +0300

    9. User

---
+++ api/v1/views/users.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""This script defines Usrs"""
+from api.v1.views import app_views
+from flask import jsonify, abort, request
+from models import storage
+from models.user import User
+
+
+@app_views.route('/users', strict_slashes=False)
+def get_all_users():
+    """Retrieves the list of all User objects"""
+    users = storage.all(User).values()
+    return jsonify([user.to_dict() for user in users])
+
+
+@app_views.route('/users/<user_id>', strict_slashes=False)
+def get_usere(user_id):
+    """Retrieves a User object"""
+    user = storage.get(User, user_id)
+    if user:
+        return jsonify(user.to_dict())
+    else:
+        return abort(404)
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def delete_usere(user_id):
+    """Delete a User object"""
+    user = storage.get(User, user_id)
+    if user:
+        storage.delete(user)
+        storage.save()
+    else:
+        return abort(404)
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def Create_usere():
+    """Create a User object"""
+    if request.content_type != 'application/json':
+        return abort(400, 'NOt a Json')
+    if not request.get_json():
+        return abort(400, 'NOt a Json')
+    data = request.get_json()
+    if 'email' not in data:
+        return abort(400, 'Missing email')
+    if 'password' not in data:
+        return abort(400, 'Missing password')
+
+    user = User(**data)
+    user.save()
+
+    return jsonify(user.to_dict()), 200
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def Update_usere(user_id):
+    """Create a User object"""
+    user = storage.get(User, user_id)
+    if user:
+        if request.content_type != 'application/json':
+            return abort(400, 'NOt a Json')
+        if not request.get_json():
+            return abort(400, 'NOt a Json')
+        data = request.get_json()
+        Ignore_keys = ['id', 'state_id', 'created_at', 'updated_at']
+        for key, value in data.items():
+            if key not in Ignore_keys:
+                setattr(user, key, value)
+        user.save()
+        return jsonify(user.to_dict()), 200
+    else:
+        return abort(404)
