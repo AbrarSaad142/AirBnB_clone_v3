3bdc1c6f0a13ea33f916d31c2aeab3e3dc4c5c39
Areejslama <Areegahmed449@gmail.com>
2024-05-28 07:02:47 -0700

    fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -29,7 +29,7 @@ def delete_state(state_id):
     state = storage.get(State, state_id)
     if state is None:
         return abort(404)
-    state.delete(state)
+    storage.delete(state)
     storage.save()
     return jsonify({}), 200
 
