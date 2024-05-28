40fdc1f562b8435b9055bd3fc02457f2985c228c
Areejslama <Areegahmed449@gmail.com>
2024-05-27 19:24:32 -0700

    edit amenity view

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -47,7 +47,7 @@ def create_amenity():
     return jsonify(new_amenity.to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'])
 def update_amenity(amenity_id):
     """Update Amenity"""
     amenity = storage.get(Amenity, amenity_id)
